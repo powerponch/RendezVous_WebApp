deeb7b3c2b30beb3535ae7a401b5e01d4b4879ad
powerponch <asandovalros@outlook.com>
2015-09-30 17:48:31 -0500

    Cambios visuales en softphone. Hay favicon wuhuu

--- RendezVousWebApp/public/assets/js/scripts.js
+++ RendezVousWebApp/public/assets/js/scripts.js
@@ -1,7 +1,11 @@
-document.getElementById("btnSend").onmouseover.src = 'assets/images/btnSend1';
-document.getElementById("btnSend").onmouseout.src = 'assets/images/btnSend2';
-document.getElementById("btnEend").onmouseover.src = 'assets/images/btnEnd1';
-document.getElementById("btnEend").onmouseout.src = 'assets/images/btnEnd2';
+
+function cargarSoftphone(){
+    document.getElementById("btnSend").onmouseover.src = 'assets/images/btnSend1';
+    document.getElementById("btnSend").onmouseout.src = 'assets/images/btnSend2';
+    document.getElementById("btnEend").onmouseover.src = 'assets/images/btnEnd1';
+    document.getElementById("btnEend").onmouseout.src = 'assets/images/btnEnd2';
+}
+
 // Función para validar que no se puedan agregar caracteres que no sean numéricos
 function validaTeclado(evt) {
 	var code = (evt.which) ? evt.which : evt.keyCode;
@@ -18,29 +22,24 @@ function validaTeclado(evt) {
 			return false;
 		}
 }
+
+
 // Función para mostrar numeros presionados en la pantalla del softphone
 function agregaTexto(Tecla) {
 	document.getElementById("pantalla").value += Tecla;
 }
 
-jQuery.$(document).ready(function () {
-    console.log("Entra funcion");
-    $("#btnSend").mouseover(function () {
-        $(this).attr("src", "/assets/images/btnSend1.png");
-    });
-    
-    $("#btnSend").mouseout(function () {
-        $(this).attr("src", "/assets/images/btnSend2.png");
-    });
-});
+function hoverRojo(element) {
+    element.setAttribute('src', 'assets/images/btnEnd1.png');
+}
+function unhoverRojo(element) {
+    element.setAttribute('src', 'assets/images/btnEnd2.png');
+}
 
-jQuery.$(document).ready(function () {
-    console.log("Entra funcion");
-    $("#btnEnd").mouseover(function () {
-        $(this).attr("src", "/assets/images/btnEnd1.png");
-    });
-    
-    $("#btnEnd").mouseout(function () {
-        $(this).attr("src", "/assets/images/btnEnd2.png");
-    });
-});
+function hoverVerde(element) {
+    element.setAttribute('src', 'assets/images/btnSend1.png');
+}
+function unhoverVerde(element) {
+    element.setAttribute('src', 'assets/images/btnSend2.png');
+}
+
